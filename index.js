7254a521729b87076f9bccc54cd9351cdb21c0f1
Gustavo Medeiros <gugamedeiros517@gmail.com>
2021-04-06 23:06:22 -0300

    Fixed connection

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const customExpress = require("./config/customExpress");
 const conexao = require("./infrastructure/conexao");
 
 conexao.connect((erro) => {
-  if (erro) {
+  if (erro)
+    console.log("Ocorreu um erro na conexao");
+  else {
     const app = customExpress;
 
     app.listen(3000, () => console.log("Servidor ativo"));
 
     console.log("A conexao deu certo!");
-  }
-  else
-    console.log("Ocorreu um erro na conexao");
+  } 
 });
