c5f92a56c5fc7cc4dd4176e1b32f7ab266b5656d
Gustavo Medeiros <gugamedeiros517@gmail.com>
2021-04-08 10:26:05 -0300

    Atendimentos table created successfully

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const customExpress = require("./config/customExpress");
 const conexao = require("./infrastructure/conexao");
-const tabelas = require("./infrastructure/tabelas");
+const Tabelas = require("./infrastructure/tabelas");
 
 conexao.connect((erro) => {
   if (erro)
     console.log(erro);
   else {
     const app = customExpress;
-
+    
     app.listen(3000, () => console.log("Servidor ativo"));
-
+    
     console.log("A conexao deu certo!");
+
+    Tabelas.init(conexao);
   } 
 });
-
-tabelas.init(conexao);
--- infrastructure/tabelas.js
+++ infrastructure/tabelas.js
@@ -1,6 +1,17 @@
 class tabelas {
+  #conexao;
+
   init(conexao) {
-    console.log(conexao);
+    this.#conexao = conexao;
+    this.criarAtendimentos();
+  }
+
+  criarAtendimentos() {
+    const sql = "CREATE TABLE `Atendimentos` (`id` INT NOT NULL,`cliente` VARCHAR(50) NOT NULL,`pet` VARCHAR(20) NOT NULL,`servico` VARCHAR(20) NOT NULL,`status` VARCHAR(20) NOT NULL, `observacoes` TEXT NOT NULL, PRIMARY KEY (`id`));"
+
+    this.#conexao.query(sql, (erro) => {
+      console.log(erro ? `${erro}` : "Tabela de atendimentos criada com sucesso");
+    });
   }
 }
 
