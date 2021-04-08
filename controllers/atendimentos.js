8935eb1b10c949dff4102b66848b8a21f150d615
Gustavo Medeiros <gugamedeiros517@gmail.com>
2021-04-08 16:34:27 -0300

    Database row insertion working

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,10 +1,14 @@
+const Atendimento = require("../models/atendimentos");
+
 module.exports = (app) => {
   const msg = "Você está na rota de atendimentos";
 
   app.get("/atendimentos", (req, res) => res.send(`${msg} e está realizando uma requisição GET`));
   
   app.post("/atendimentos", (req, res) => {
-    console.log(req.body);
+    const atendimento = req.body;
+
+    Atendimento.adiciona(atendimento);
 
     res.send(`${msg} e está realizando uma requisição POST`)
   });
--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const customExpress = require("./config/customExpress");
 const conexao = require("./infrastructure/conexao");
 const Tabelas = require("./infrastructure/tabelas");
+const Atendimento = require("./models/atendimentos")
 
 conexao.connect((erro) => {
   if (erro)
     console.log(erro);
   else {
+    console.log("A conexao deu certo");
+
     const app = customExpress();
     
     app.listen(3000, () => console.log("Servidor ativo"));
     
-    console.log("A conexao deu certo!");
-
     Tabelas.init(conexao);
   } 
 });
