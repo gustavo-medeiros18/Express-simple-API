e702cd2c0bf421a2f5467b7f95fd8c727abd889b
Gustavo Medeiros <gugamedeiros517@gmail.com>
2021-07-16 15:36:26 -0300

    some unnecessary files were modified/deleted

--- database/tabelas.js
+++ database/tabelas.js
@@ -7,7 +7,7 @@ class tabelas {
   }
 
   criarAtendimentos() {
-    const sql = "CREATE TABLE IF NOT EXISTS`Atendimentos` (`id` INT NOT NULL,`cliente` VARCHAR(50) NOT NULL,`pet` VARCHAR(20) NOT NULL,`servico` VARCHAR(20) NOT NULL,`data` DATETIME NOT NULL, `dataCriacao` DATETIME NOT NULL, `status` VARCHAR(20) NOT NULL, `observacoes` TEXT NOT NULL, PRIMARY KEY (`id`));"
+    const sql = "CREATE TABLE IF NOT EXISTS `Atendimentos` (`id` INT NOT NULL,`cliente` VARCHAR(50) NOT NULL,`pet` VARCHAR(20) NOT NULL,`servico` VARCHAR(20) NOT NULL,`data` DATETIME NOT NULL, `dataCriacao` DATETIME NOT NULL, `status` VARCHAR(20) NOT NULL, `observacoes` TEXT NOT NULL, PRIMARY KEY (`id`));"
 
     this.#conexao.query(sql, (erro) => {
       console.log(erro ? `${erro}` : "Tabela de atendimentos criada com sucesso");
